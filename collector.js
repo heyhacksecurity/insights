6eecbb5e9e82204a9dc4fa52f0382fd29ce67bc8
Anders Skovsgaard <anders@columno.com>
2022-01-31 14:16:23 +0100

    Create collector.js

---
+++ collector.js
@@ -0,0 +1,16 @@
+// HEYHACK JAVASCRIPT COLLECTOR V 0.01
+// Currently, only collecting URLs
+var heyhackdatainsight = sessionStorage.getItem("HeyHackInsightsData");
+
+if(heyhackdatainsight == null) {
+ sessionStorage.setItem('HeyHackInsightsData', btoa(window.location.href));
+} else if(heyhackdatainsight.search(btoa(window.location.href)) == -1) {
+  sessionStorage.setItem('HeyHackInsightsData', btoa(window.location.href) + ";" + heyhackdatainsight);
+}
+
+if(Math.floor(Math.random() * 10)%10==0) {
+  var xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://insight.heyhack.com/collect?data=" + sessionStorage.getItem("HeyHackInsightsData"), true);
+	xhr.send();
+	sessionStorage.removeItem('HeyHackInsightsData');
+}
